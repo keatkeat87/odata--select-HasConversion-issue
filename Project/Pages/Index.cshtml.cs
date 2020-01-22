2b3ca0796835b111d1a3cfe984cc83b4dd66538b
兴杰 严 <keatkeat87@live.com>
2020-01-22 22:24:42 +0800

    ok

--- Project/Pages/Index.cshtml.cs
+++ Project/Pages/Index.cshtml.cs
@@ -20,18 +20,18 @@ namespace Project.Pages
 
         public void OnGet([FromServices] CustomerOrderContext Db)
         {
-            //Db.Products.Add(new Product
-            //{
-            //    name = "mk100",
-            //    image = null,
-            //    images = new List<Image> { new Image { src = "da", size = 5 }, new Image { src = "da", size = 5 } }
-            //});
-            //Db.Products.Add(new Product
-            //{
-            //    name = "mk200",
-            //    image = new Image { src = "aa", size = 6 }
-            //});
-            //Db.SaveChanges();
+            Db.Products.Add(new Product
+            {
+                name = "mk100",
+                image = null,
+                images = new List<Image> { new Image { src = "da", size = 5 }, new Image { src = "da", size = 5 } }
+            });
+            Db.Products.Add(new Product
+            {
+                name = "mk200",
+                image = new Image { src = "aa", size = 6 }
+            });
+            Db.SaveChanges();
         }
     }
 }
